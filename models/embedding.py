5404b56f73b14d62510a65d4929a8b9b02384880
ZYKLOBS <danyalturpcu@gmail.com>
2025-01-26 15:18:58 +0100

    added code to test openAI embeddings, waiting for key...

--- models/embedding.py
+++ models/embedding.py
@@ -7,3 +7,7 @@ def get_embedding_function(model:str = config_embedding):
     #llama3:70b parameter
     embeddings = OllamaEmbeddings(model=model)
     return embeddings
+
+#def get_embedding_function(model:str = "text-embedding-3-large"):
+#    embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("API_KEY"), model="text-embedding-3-large")
+#    return embeddings
