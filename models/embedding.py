034aedd1b0d2f3cb8b12952a4d9ccd9e57918d61
ZYKLOBS <danyalturpcu@gmail.com>
2025-01-27 13:43:48 +0100

    Added Open AI tests + restructured test folder

--- models/embedding.py
+++ models/embedding.py
@@ -1,13 +1,16 @@
 from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_community.embeddings.openai import OpenAIEmbeddings
+import os
+
 from config.settings import config_embedding
 #bge-m3
 #nomic-embed-text
 #mxbai-embed-large
-def get_embedding_function(model:str = config_embedding):
-    #llama3:70b parameter
-    embeddings = OllamaEmbeddings(model=model)
-    return embeddings
-
-#def get_embedding_function(model:str = "text-embedding-3-large"):
-#    embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("API_KEY"), model="text-embedding-3-large")
+#def get_embedding_function(model:str = config_embedding):
+#    #llama3:70b parameter
+#    embeddings = OllamaEmbeddings(model=model)
 #    return embeddings
+
+def get_embedding_function(model:str = "text-embedding-3-large"):
+    embeddings = OpenAIEmbeddings(openai_api_key=os.getenv('API_KEY'), model="text-embedding-3-large")
+    return embeddings
